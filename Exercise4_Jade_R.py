e050e3db5da3b1beabfee7073de0c8dd36327f43
NewsPapa <111194961+NewsPapa@users.noreply.github.com>
2022-08-20 21:37:17 +0700

    Exercise4_Jade_R.py

---
+++ Exercise4_Jade_R.py
@@ -0,0 +1,14 @@
+FE = 3.5
+GB = 2
+IS = 3
+CP = 4
+print("-------- Your Score --------")
+print("Foundation English >>",FE)
+print("General Business >>",GB)
+print("Introduction to Computer Systems >>",IS)
+print("Computer Programming >>",CP)
+
+#Foundation English
+#General Business
+#Introduction to Computer Systems
+#Computer Programming
