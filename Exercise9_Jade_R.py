ea3f6985e90f4d24b539dc865d1a822eedfde462
NewsPapa <111194961+NewsPapa@users.noreply.github.com>
2022-08-21 22:45:44 +0700

    Create Exercise9_Jade_R.py

---
+++ Exercise9_Jade_R.py
@@ -0,0 +1,6 @@
+UserName = input("Username :")
+PassWord = input("Password :")
+while UserName != "Admin" or PassWord != "1234":
+  UserName = input("Username :")
+  PassWord = input("Password :")
+print("DONE")
